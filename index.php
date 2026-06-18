942001f76775110b44f2d2b8665a271cf69ab4ef
Student <Student@LabVM2049826>
2026-06-18 20:24:11 +0200

    changed the minimum length requirement for the password to 8 characters in the login script

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 <!--Asking The User For Password, Creating The Password Label And Input Field-->
 <label>Password: </label>
-<input type="password" name="password" required> <br><br>
+<input type="password" name="password" required minlength="8"> <br><br>
 
 <!--Button For Input-->
 <input type="submit" name="login" value="Login">
