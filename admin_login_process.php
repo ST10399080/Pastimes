38e3d3f01506d9c03f90aefd61d253a4fd1ab420
Student <Student@LabVM2049826>
2026-06-19 19:35:31 +0200

    created new scripts for admin_dashboard.php, admin_login_process.php, delete_user, edit_user.php and manage_users.php also coded a login page in the admin_login.php

---
+++ admin_login_process.php
@@ -0,0 +1,8 @@
+<form action="admin_login_process.php" method="post">
+
+<input type="email" name="email" placeholder="Admin Email" required>
+<input type="password" name="password" placeholder="Password" required>
+
+<button type="submit">Login as Admin</button>
+
+</form>
