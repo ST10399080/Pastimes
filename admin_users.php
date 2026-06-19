6c838574afab7370881c019db80a092add2ab379
Student <Student@LabVM2049826>
2026-06-19 13:55:16 +0200

    created a new script called admin_users.php and added code in it

---
+++ admin_users.php
@@ -0,0 +1,24 @@
+<?php
+
+include "DBConn.php";
+
+$query = "SELECT * FROM tbluser WHERE status='pending'";
+
+$result = mysqli_query($conn, $query);
+
+while($user = mysqli_fetch_assoc($result)){
+
+    echo $user['userName'];
+    echo " | ";
+    echo $user['userEmail'];
+
+    echo "
+    <a href='approve_user.php?id={$user['userID']}'>Approve</a>
+
+    <a href='reject_user.php?id={$user['userID']}'>Reject</a>
+
+    <br><br>
+    ";
+}
+
+?>
